7584c1db6a250d6617ef734de0419dd45bb609f4
savileaf <it@savileaf.com.au>
2025-07-24 17:06:26 +0545

    Added data to the viewgroup page

--- src/components/assests/ViewGroupPage.tsx
+++ src/components/assests/ViewGroupPage.tsx
@@ -24,20 +24,31 @@ const customColumns = [
   {title:"SIZE", dataIndex:"size", key:"size", width:100}
 ];
 
+const data =[
+  { key: "1", group_name: "Most Used Products", created_date: "2025/03/03", size: "2mb" },
+    { key: "2", group_name: "New Products", created_date: "2025/04/10", size: "1.5mb" },
+    { key: "3", group_name: "Jeans", created_date: "2025/04/12", size: "3mb" },
+    { key: "4", group_name: "Ladies Items", created_date: "2025/03/03", size: "2mb" },
+     { key: "5", group_name: "New Products", created_date: "2025/04/10", size: "1.5mb" },
+    { key: "6", group_name: "Jeans", created_date: "2025/04/12", size: "3mb" },
+    { key: "7", group_name: "Ladies Items", created_date: "2025/03/03", size: "2mb" },
+]
+
+
 
 
 const ViewGroupPage = () => {
-const {groups} = useGroupContext()
-const tableData = groups.map((group, index) => ({
-  key: index.toString(),
-  group_name: group.name,
-  created_date: new Date().toLocaleDateString(), 
-  size: `${group.attributes.length} assets`,
-}));
+// const {groups} = useGroupContext()
+// const tableData = groups.map((group, index) => ({
+//   key: index.toString(),
+//   group_name: group.name,
+//   created_date: new Date().toLocaleDateString(), 
+//   size: `${group.attributes.length} assets`,
+// }));
   return (
     <div className=''>
         <AssetsTopbar />
-        <CustomTable columns={customColumns} showImage={false} dataSource={tableData}/>
+        <CustomTable columns={customColumns} showImage={false} dataSource={data}/>
     </div>
   )
 }
